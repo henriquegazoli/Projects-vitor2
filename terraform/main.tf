c39245e6787a659fe1a80d55e72bbe0307ba52c8
Jose vitor <jose.vitor.oliveira.jvo@gmail.com>
2026-06-02 16:08:27 -0300

    colocado modulos dev e prod

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
-module "s3" {
+module "dev" {
   source = "./modules/s3" 
   bucket_name = "bucket-dev-vitor-hgm"
-  tag_projeto = "Projeto S3"
+  tag_projeto = "Projeto dev"
+}
+
+module "prod" {
+  source = "./modules/s3" 
+  bucket_name = "bucket-prod-vitor-hgm"
+  tag_projeto = "Projeto prod"
 }
